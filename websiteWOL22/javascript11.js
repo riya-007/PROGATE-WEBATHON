0f189de2158d43c6c44adbd6634912ecdbfc4532
riya-007 <63338189+riya-007@users.noreply.github.com>
2020-04-08 13:20:50 +0530

    Add files via upload
    PROGATE WEBATHON PROJECT.

---
+++ websiteWOL22/javascript11.js
@@ -0,0 +1,18 @@
+document.querySelector('.btn-wrapper').addEventListener ('click' , (event)=>  {
+	event.target.textContent = ' (Sign up / Log in) first'
+})
+
+
+document.getElementById('button').addEventListener('click' , function() {
+	
+
+document.querySelector('.popup').style.display ='flex';
+});
+
+
+
+document.querySelector('.close').addEventListener('click' , function() {
+
+document.querySelector('.popup').style.display ='none';
+
+});
